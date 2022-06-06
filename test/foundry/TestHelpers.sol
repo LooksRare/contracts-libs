950703be56838632b6ed80c23bbff50fcbd6d7e7
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-06-06 22:00:42 +0900

    test: Remove ICheatCodes

--- test/foundry/TestHelpers.sol
+++ test/foundry/TestHelpers.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ICheatCodes} from "./ICheatCodes.sol";
 import {Test} from "../../lib/forge-std/src/Test.sol";
 
 abstract contract TestHelpers is Test {
