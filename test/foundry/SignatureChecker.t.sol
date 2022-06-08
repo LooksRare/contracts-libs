fe00077e7382ed54cfba268c55735dd7842091ec
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-06-08 11:28:59 +0900

    test: More tests

--- test/foundry/SignatureChecker.t.sol
+++ test/foundry/SignatureChecker.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.14;
 
 import {SignatureChecker} from "../../contracts/SignatureChecker.sol";
 import {TestHelpers} from "./TestHelpers.sol";
+import {ERC1271Contract} from "./utils/ERC1271Contract.sol";
 
 abstract contract SignatureCheckerErrors {
     error BadSignatureS();
@@ -21,6 +22,9 @@ abstract contract TestParameters is TestHelpers {
     address internal user1 = vm.addr(privateKeyUser1);
     address internal user2 = vm.addr(privateKeyUser2);
 
+    // Random message signed
+    bytes32 internal _message = keccak256("Hello World");
+
     function _signMessage(bytes32 message, uint256 _key) internal returns (bytes memory) {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(_key, keccak256(abi.encodePacked(message)));
         return abi.encodePacked(r, s, v);
@@ -31,7 +35,7 @@ abstract contract TestParameters is TestHelpers {
     }
 }
 
-contract GreatSignatureChecker is SignatureChecker {
+contract PublicSignatureChecker is SignatureChecker {
     function recoverEOASigner(bytes32 hash, bytes memory signature) external pure returns (address) {
         return _recoverEOASigner(hash, signature);
     }
@@ -48,18 +52,39 @@ contract GreatSignatureChecker is SignatureChecker {
 }
 
 contract SignatureCheckerTest is TestHelpers, TestParameters, SignatureCheckerErrors {
-    GreatSignatureChecker public signatureChecker;
+    PublicSignatureChecker public signatureChecker;
 
     function setUp() public {
-        signatureChecker = new GreatSignatureChecker();
+        signatureChecker = new PublicSignatureChecker();
     }
 
     function testSignEOA() public {
-        bytes32 message = keccak256("Hello World");
-        bytes memory signature = _signMessage(message, privateKeyUser1);
-        bytes32 hashedMessage = _computeHash(message);
+        bytes memory signature = _signMessage(_message, privateKeyUser1);
+        bytes32 hashedMessage = _computeHash(_message);
 
         assertEq(signatureChecker.recoverEOASigner(hashedMessage, signature), user1);
         assertTrue(signatureChecker.verify(hashedMessage, user1, signature));
+    }
+
+    function testSignERC1271() public {
+        ERC1271Contract erc1271Contract = new ERC1271Contract(user1);
+        bytes memory signature = _signMessage(_message, privateKeyUser1);
+        bytes32 hashedMessage = _computeHash(_message);
+        assertTrue(signatureChecker.verify(hashedMessage, address(erc1271Contract), signature));
+    }
+
+    function testCannotSignIfWrongSignatureERC1271() public {
+        ERC1271Contract erc1271Contract = new ERC1271Contract(user1);
+        bytes memory signature = _signMessage(_message, privateKeyUser2);
+        bytes32 hashedMessage = _computeHash(_message);
+        vm.expectRevert(InvalidSignatureERC1271.selector);
+        signatureChecker.verify(hashedMessage, address(erc1271Contract), signature);
+    }
+
+    function testCannotSignIfWrongSignatureEOA() public {
+        bytes memory signature = _signMessage(_message, privateKeyUser2);
+        bytes32 hashedMessage = _computeHash(_message);
+        vm.expectRevert(InvalidSignatureEOA.selector);
+        signatureChecker.verify(hashedMessage, user1, signature);
     }
 }
