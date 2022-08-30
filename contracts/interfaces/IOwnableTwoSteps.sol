034b10770e3dfd45abf35c0dd08b237e1145b49a
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-08-30 15:44:56 +0900

    feat: Timelock for renouncing ownership and refactoring

---
+++ contracts/interfaces/IOwnableTwoSteps.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+/**
+ * @title IOwnableTwoSteps
+ */
+contract IOwnableTwoSteps {
+    enum Status {
+        NoOngoingTransfer,
+        TransferInProgress,
+        RenouncementInProgress
+    }
+
+    // Custom errors
+    error NoOngoingTransferInProgress();
+    error NotOwner();
+    error RenouncementTooEarly();
+    error RenouncementNotInProgress();
+    error TransferAlreadyInProgress();
+    error TransferNotInProgress();
+    error WrongPotentialOwner();
+
+    // Events
+    event CancelOwnershipTransfer();
+    event InitiateOwnershipRenouncement();
+    event InitiateOwnershipTransfer(address previousOwner, address potentialOwner);
+    event NewOwner(address newOwner);
+}
