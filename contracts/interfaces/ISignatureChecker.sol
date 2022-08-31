07e347be0f6dab30437b3bf353c10a2dafd39010
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-08-31 10:58:40 +0900

    refactor: Interfaces

---
+++ contracts/interfaces/ISignatureChecker.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ISignatureChecker {
+    // Custom errors
+    error BadSignatureS();
+    error BadSignatureV(uint8 v);
+    error InvalidSignatureERC1271();
+    error InvalidSignatureEOA();
+    error NullSignerAddress();
+    error WrongSignatureLength(uint256 length);
+}
