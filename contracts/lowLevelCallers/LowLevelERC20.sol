d98a091750449b0ff1671ff59c590939a91facab
hiroshi <hiroshi@looksrare.org>
2022-10-06 13:33:57 +0100

    feat: Low level ERC-20 approve

--- contracts/lowLevelCallers/LowLevelERC20.sol
+++ contracts/lowLevelCallers/LowLevelERC20.sol
@@ -9,8 +9,28 @@ import {IERC20} from "../interfaces/generic/IERC20.sol";
  * @author LooksRare protocol team (👀,💎)
  */
 contract LowLevelERC20 {
+    error ERC20ApprovalFail();
     error ERC20TransferFail();
     error ERC20TransferFromFail();
+
+    /**
+     * @notice Execute ERC20 approve
+     * @param currency Currency address
+     * @param to Operator address
+     * @param amount Amount to approve
+     */
+    function _executeERC20Approve(
+        address currency,
+        address to,
+        uint256 amount
+    ) internal {
+        (bool status, bytes memory data) = currency.call(abi.encodeWithSelector(IERC20.approve.selector, to, amount));
+
+        if (!status) revert ERC20ApprovalFail();
+        if (data.length > 0) {
+            if (!abi.decode(data, (bool))) revert ERC20ApprovalFail();
+        }
+    }
 
     /**
      * @notice Execute ERC20 transferFrom
--- test/foundry/LowLevelERC20.t.sol
+++ test/foundry/LowLevelERC20.t.sol
@@ -6,6 +6,14 @@ import {MockERC20} from "../mock/MockERC20.sol";
 import {TestHelpers} from "./utils/TestHelpers.sol";
 
 contract ImplementedLowLevelERC20 is LowLevelERC20 {
+    function approveERC20(
+        address currency,
+        address to,
+        uint256 amount
+    ) external {
+        _executeERC20Approve(currency, to, amount);
+    }
+
     function transferERC20(
         address currency,
         address to,
@@ -27,6 +35,7 @@ contract ImplementedLowLevelERC20 is LowLevelERC20 {
 abstract contract TestParameters {
     address internal _sender = address(100);
     address internal _recipient = address(101);
+    address internal _operator = address(102);
 }
 
 contract LowLevelERC20Test is TestHelpers, TestParameters {
@@ -36,6 +45,11 @@ contract LowLevelERC20Test is TestHelpers, TestParameters {
     function setUp() external {
         lowLevelERC20 = new ImplementedLowLevelERC20();
         mockERC20 = new MockERC20();
+    }
+
+    function testApproveERC20(uint256 amount) external {
+        lowLevelERC20.approveERC20(address(mockERC20), _operator, amount);
+        assertEq(mockERC20.allowance(address(lowLevelERC20), _operator), amount);
     }
 
     function testTransferFromERC20(uint256 amount) external asPrankedUser(_sender) {
