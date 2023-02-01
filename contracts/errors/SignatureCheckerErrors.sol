2e68bd27b373f5b13a340eb0ef50549b042b3f7c
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2023-02-01 16:07:08 +0900

    refactor: SignatureCheckerErrors

--- contracts/errors/SignatureCheckerErrors.sol
+++ contracts/errors/SignatureCheckerErrors.sol
@@ -2,6 +2,26 @@
 pragma solidity ^0.8.17;
 
 /**
+ * @notice It is emitted if the signer is null.
+ */
+error NullSignerAddress();
+
+/**
+ * @notice It is emitted if the signature is invalid for an EOA (the address recovered is not the expected one).
+ */
+error SignatureEOAInvalid();
+
+/**
+ * @notice It is emitted if the signature is invalid for a ERC1271 contract signer.
+ */
+error SignatureERC1271Invalid();
+
+/**
+ * @notice It is emitted if the signature's length is neither 64 nor 65 bytes.
+ */
+error SignatureLengthInvalid(uint256 length);
+
+/**
  * @notice It is emitted if the signature is invalid due to S parameter.
  */
 error SignatureParameterSInvalid();
@@ -10,23 +30,3 @@ error SignatureParameterSInvalid();
  * @notice It is emitted if the signature is invalid due to V parameter.
  */
 error SignatureParameterVInvalid(uint8 v);
-
-/**
- * @notice It is emitted if the signature is invalid for a ERC1271 contract signer.
- */
-error SignatureERC1271Invalid();
-
-/**
- * @notice It is emitted if the signature is invalid for an EOA (the address recovered is not the expected one).
- */
-error SignatureEOAInvalid();
-
-/**
- * @notice It is emitted if the signer is null.
- */
-error NullSignerAddress();
-
-/**
- * @notice It is emitted if the signature's length is not 64 or 65 bytes.
- */
-error SignatureLengthInvalid(uint256 length);
