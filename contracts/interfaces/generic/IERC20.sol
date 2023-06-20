3a0e70488ddbb3ac3a81dc5b274332160c833431
hiroshi <hiroshi@looksrare.org>
2023-06-20 15:30:26 +0100

    feat: IERC20 decimals

--- contracts/interfaces/generic/IERC20.sol
+++ contracts/interfaces/generic/IERC20.sol
@@ -17,4 +17,6 @@ interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
 
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
+
+    function decimals() external view returns (uint8);
 }
