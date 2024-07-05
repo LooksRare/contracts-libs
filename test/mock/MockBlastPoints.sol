06cebd1c7dace679983cf18aca0ef8782fbbe985
stonehenge <stonehenge@looksrare.org>
2024-07-05 19:36:04 +0400

    fix: Remove event

--- test/mock/MockBlastPoints.sol
+++ test/mock/MockBlastPoints.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 contract MockBlastPoints {
     mapping(address _contract => address operator) public contractOperators;
-    event LogAddress(address);
 
     function configurePointsOperator(address operator) external {
         contractOperators[msg.sender] = operator;
