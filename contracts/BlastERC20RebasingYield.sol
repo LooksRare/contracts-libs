534d0539f114da5905628eb7d036ac3cbdb3c38d
stonehenge <stonehenge@looksrare.org>
2024-07-06 08:36:49 +0400

    fix: Fix _claim notice

--- contracts/BlastERC20RebasingYield.sol
+++ contracts/BlastERC20RebasingYield.sol
@@ -38,7 +38,7 @@ contract BlastERC20RebasingYield is BlastNativeYield {
     }
 
     /**
-     * @notice Claim Blast yield. Guarding of the function is dependent on the inherited contract. Inheriting does not allow claiming by default. A public or external function is required in the child contract to access the Claim function.
+     * @notice Claim Blast yield. Guarding of the function is dependent on the inherited contract. Inheriting does not allow claiming by default. A public or external function is required in the child contract to access the _claim function.
      * @param wethReceiver The receiver of WETH.
      * @param usdbReceiver The receiver of USDB.
      */
